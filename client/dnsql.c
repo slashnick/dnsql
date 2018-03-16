a07e5c9163798d189de7515f19c1f22cfb062305
Nick Frost <nickfrostatx@gmail.com>
2018-03-15 17:36:38 -0700

    make format

--- client/dnsql.c
+++ client/dnsql.c
@@ -27,12 +27,14 @@ static int vfs_open(sqlite3_vfs *, const char *, sqlite3_file *, int, int *);
 static int vfs_delete(sqlite3_vfs *, const char *, int);
 static int vfs_access(sqlite3_vfs *, const char *, int, int *);
 
-typedef struct { sqlite3_vfs *parent; } context;
+typedef struct {
+    sqlite3_vfs *parent;
+} context;
 
 typedef struct {
-  sqlite3_file base;
-  PyObject *module;
-  PyObject *class;
+    sqlite3_file base;
+    PyObject *module;
+    PyObject *class;
 } dnsql_file;
 
 static const sqlite3_io_methods dnsql_io_methods = {
@@ -57,63 +59,69 @@ static const sqlite3_io_methods dnsql_io_methods = {
     NULL                        /* xUnfetch */
 };
 
-int vfs_close(sqlite3_file *file) { return SQLITE_OK; }
+int vfs_close(sqlite3_file *file) {
+    return SQLITE_OK;
+}
 
 int vfs_read(sqlite3_file *pFile, void *zBuf, int iAmt, sqlite_int64 iOfst) {
-  PyObject *result;
-  char *result_buffer;
-  Py_ssize_t result_size;
-  dnsql_file *file = (dnsql_file *)pFile;
+    PyObject *result;
+    char *result_buffer;
+    Py_ssize_t result_size;
+    dnsql_file *file = (dnsql_file *)pFile;
 
-  result = PyObject_CallMethod(file->class, "read", "(O,l,i)", file->class,
-                               iOfst, iAmt);
+    result = PyObject_CallMethod(file->class, "read", "(O,l,i)", file->class,
+                                 iOfst, iAmt);
 
-  if (result == NULL) {
-    PyErr_Print();
-    fprintf(stderr, "Failed to call read\n");
-    return SQLITE_ERROR;
-  }
+    if (result == NULL) {
+        PyErr_Print();
+        fprintf(stderr, "Failed to call read\n");
+        return SQLITE_ERROR;
+    }
 
-  if (PyBytes_AsStringAndSize(result, &result_buffer, &result_size) < 0) {
+    if (PyBytes_AsStringAndSize(result, &result_buffer, &result_size) < 0) {
+        Py_DECREF(result);
+        PyErr_Print();
+        fprintf(stderr, "Failed to extract read result\n");
+        return SQLITE_ERROR;
+    }
+
+    if (result_size != iAmt) {
+        Py_DECREF(result);
+        fprintf(stderr, "Mismatch in read result vs buffer sizes\n");
+        return SQLITE_ERROR;
+    }
+
+    memcpy(zBuf, result_buffer, iAmt);
+
     Py_DECREF(result);
-    PyErr_Print();
-    fprintf(stderr, "Failed to extract read result\n");
-    return SQLITE_ERROR;
-  }
-
-  if (result_size != iAmt) {
-    Py_DECREF(result);
-    fprintf(stderr, "Mismatch in read result vs buffer sizes\n");
-    return SQLITE_ERROR;
-  }
-
-  memcpy(zBuf, result_buffer, iAmt);
-
-  Py_DECREF(result);
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int vfs_write(sqlite3_file *pFile, const void *zBuf, int iAmt,
               sqlite_int64 iOfst) {
-  PyObject *result;
-  dnsql_file *file = (dnsql_file *)pFile;
+    PyObject *result;
+    dnsql_file *file = (dnsql_file *)pFile;
 
-  result = PyObject_CallMethod(file->class, "write", "(O, y#, l)", file->class,
-                               zBuf, iAmt, iOfst);
+    result = PyObject_CallMethod(file->class, "write", "(O, y#, l)",
+                                 file->class, zBuf, iAmt, iOfst);
 
-  if (result == NULL) {
-    PyErr_Print();
-    fprintf(stderr, "Failed to call write\n");
-    return 1;
-  }
+    if (result == NULL) {
+        PyErr_Print();
+        fprintf(stderr, "Failed to call write\n");
+        return 1;
+    }
 
-  Py_DECREF(result);
-  return SQLITE_OK;
+    Py_DECREF(result);
+    return SQLITE_OK;
 }
 
-int vfs_truncate(sqlite3_file *pFile, sqlite_int64 size) { return SQLITE_OK; }
+int vfs_truncate(sqlite3_file *pFile, sqlite_int64 size) {
+    return SQLITE_OK;
+}
 
-int vfs_sync(sqlite3_file *pFile, int flags) { return SQLITE_OK; }
+int vfs_sync(sqlite3_file *pFile, int flags) {
+    return SQLITE_OK;
+}
 
 int vfs_file_size(sqlite3_file *pFile, sqlite_int64 *pSize) {
     PyObject *size_attr;
@@ -121,7 +129,7 @@ int vfs_file_size(sqlite3_file *pFile, sqlite_int64 *pSize) {
 
     size_attr = PyObject_GetAttrString(file->class, "size");
 
-    if(size_attr == NULL) {
+    if (size_attr == NULL) {
         PyErr_Print();
         fprintf(stderr, "Failed to get size from object");
         return SQLITE_ERROR;
@@ -131,93 +139,93 @@ int vfs_file_size(sqlite3_file *pFile, sqlite_int64 *pSize) {
     return SQLITE_OK;
 }
 
-int vfs_lock(sqlite3_file *pFile, int eLock) { return SQLITE_OK; }
+int vfs_lock(sqlite3_file *pFile, int eLock) {
+    return SQLITE_OK;
+}
 
-int vfs_unlock(sqlite3_file *pFile, int eLock) { return SQLITE_OK; }
+int vfs_unlock(sqlite3_file *pFile, int eLock) {
+    return SQLITE_OK;
+}
 
 int vfs_check_reserved_lock(sqlite3_file *pFile, int *pResOut) {
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int vfs_file_control(sqlite3_file *pFile, int op, void *pArg) {
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int vfs_sector_size(sqlite3_file *pFile) {
-  // TODO: double-check this
-  return 1 << 14;
+    // TODO: double-check this
+    return 1 << 14;
 }
 
 int vfs_device_characteristics(sqlite3_file *pFile) {
-  // TODO
-  return SQLITE_IOCAP_ATOMIC16K;
+    // TODO
+    return SQLITE_IOCAP_ATOMIC16K;
 }
 
 int vfs_open(sqlite3_vfs *pVfs, const char *zName, sqlite3_file *pFile,
              int flags, int *pOutFlags) {
-  dnsql_file *file;
-  PyObject *sys_path, *dot_str, *module, *DnsVfs_class;
+    dnsql_file *file;
+    PyObject *sys_path, *dot_str, *module, *DnsVfs_class;
 
-  printf("dnsql_open\n");
+    Py_Initialize();
 
-  Py_Initialize();
+    sys_path = PySys_GetObject("path");
 
-  sys_path = PySys_GetObject("path");
+    if (sys_path == NULL) {
+        PyErr_Print();
+        fprintf(stderr, "Failed to get system path");
+        return SQLITE_ERROR;
+    }
 
-  if (sys_path == NULL) {
-    PyErr_Print();
-    fprintf(stderr, "Failed to get system path");
-    return SQLITE_ERROR;
-  }
+    dot_str = Py_BuildValue("s", ".");
+    // Implicit Py_INCREF(prog_name)
 
-  dot_str = Py_BuildValue("s", ".");
-  // Implicit Py_INCREF(prog_name)
+    if (dot_str == NULL) {
+        PyErr_Print();
+        fprintf(stderr, "Failed to build dot string");
+        return SQLITE_ERROR;
+    }
 
-  if (dot_str == NULL) {
-    PyErr_Print();
-    fprintf(stderr, "Failed to build dot string");
-    return SQLITE_ERROR;
-  }
+    PyList_Append(sys_path, dot_str);
+    Py_DECREF(dot_str);
 
-  PyList_Append(sys_path, dot_str);
-  Py_DECREF(dot_str);
+    module = PyImport_ImportModule("dnsmap");
+    // Implicit Py_INCREF(module)
 
-  module = PyImport_ImportModule("dnsmap");
-  // Implicit Py_INCREF(module)
+    if (module == NULL) {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load script\n");
+        return SQLITE_ERROR;
+    }
 
-  if (module == NULL) {
-    PyErr_Print();
-    fprintf(stderr, "Failed to load script\n");
-    return SQLITE_ERROR;
-  }
+    DnsVfs_class = PyObject_GetAttrString(module, "DnsVfs");
+    // Implicit Py_INCREF(DnsVfs_class)
 
-  DnsVfs_class = PyObject_GetAttrString(module, "DnsVfs");
-  // Implicit Py_INCREF(DnsVfs_class)
-  fprintf(stderr, "Finished openning\n");
+    if (DnsVfs_class == NULL) {
+        Py_DECREF(DnsVfs_class);
+        Py_DECREF(module);
+        PyErr_Print();
+        fprintf(stderr, "Could not load viable function\n");
+        return SQLITE_ERROR;
+    }
 
-  if (DnsVfs_class == NULL) {
-    Py_DECREF(DnsVfs_class);
-    Py_DECREF(module);
-    PyErr_Print();
-    fprintf(stderr, "Could not load viable function\n");
-    return SQLITE_ERROR;
-  }
+    file = (dnsql_file *)pFile;
+    file->base.pMethods = &dnsql_io_methods;
+    file->module = module;
+    file->class = DnsVfs_class;
 
-  file = (dnsql_file *)pFile;
-  file->base.pMethods = &dnsql_io_methods;
-  file->module = module;
-  file->class = DnsVfs_class;
-
-
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int vfs_delete(sqlite3_vfs *pVfs, const char *zName, int syncDir) {
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int vfs_access(sqlite3_vfs *pVfs, const char *zName, int flags, int *pResOut) {
-  return SQLITE_OK;
+    return SQLITE_OK;
 }
 
 int dnsql_init() {
@@ -243,8 +251,8 @@ int dnsql_init() {
 
 int sqlite3_dnsql_init(sqlite3 *db, char **pzErrMsg,
                        const sqlite3_api_routines *pApi) {
-  SQLITE_EXTENSION_INIT2(pApi);
-  ((void)db);
-  ((void)pzErrMsg);
-  return dnsql_init();
+    SQLITE_EXTENSION_INIT2(pApi);
+    ((void)db);
+    ((void)pzErrMsg);
+    return dnsql_init();
 }
